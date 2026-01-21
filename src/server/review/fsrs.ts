cf161c09731fae2a2b1b782e32d673d50558b925
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-21 13:58:01 -0500

    Add ts-fsrs for srs logic and create new cards

---
+++ src/server/review/fsrs.ts
@@ -0,0 +1,21 @@
+import { Rat } from "lucide-react";
+import { FSRS, Rating, Card, FSRSParameters, generatorParameters} from "ts-fsrs";
+
+type FSRSRating = Exclude<Rating, Rating.Manual>;
+
+const params: FSRSParameters = generatorParameters({
+  request_retention: 0.85,
+  maximum_interval: 365,
+  enable_fuzz: true
+})
+const fsrs = new FSRS(params);
+
+/**
+ * Update a card using FSRS
+ */
+export function reviewCard(card: Card, rating: FSRSRating, now = new Date()) : Card {
+  const schedulingCards = fsrs.repeat(card, now);
+
+  return schedulingCards[rating].card;
+}
+
