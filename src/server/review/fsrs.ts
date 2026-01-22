70b215e6e32dba87ce9b021ed67e2841dcf7e422
Piero Coronado Flores <pacoronado01@gmail.com>
2026-01-21 19:06:32 -0500

    Update formatting

--- src/server/review/fsrs.ts
+++ src/server/review/fsrs.ts
@@ -1,20 +1,29 @@
-import { FSRS, Rating, Card, FSRSParameters, generatorParameters} from "ts-fsrs";
+import {
+  FSRS,
+  Rating,
+  Card,
+  FSRSParameters,
+  generatorParameters,
+} from "ts-fsrs";
 
-type FSRSRating = Exclude<Rating, Rating.Manual>;
+export type FSRSRating = Exclude<Rating, Rating.Manual>;
 
 const params: FSRSParameters = generatorParameters({
   request_retention: 0.85,
   maximum_interval: 365,
-  enable_fuzz: true
-})
+  enable_fuzz: true,
+});
 const fsrs = new FSRS(params);
 
 /**
  * Update a card using FSRS
  */
-export function reviewCard(card: Card, rating: FSRSRating, now = new Date()) : Card {
+export function reviewCard(
+  card: Card,
+  rating: FSRSRating,
+  now = new Date(),
+): Card {
   const schedulingCards = fsrs.repeat(card, now);
 
   return schedulingCards[rating].card;
 }
-
